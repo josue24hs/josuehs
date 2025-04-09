ab2e9780c662fd869b5e38f489be1a55042e254f
josue24hs <josue24hs@gmail.com>
2025-04-09 13:21:38 -0600

    Configuración para producción

--- login/settings.py
+++ login/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fvta=0b4g^^)@+p3=6=g0sov1v1o9upu*y(ua^v3h3309uc4xm'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-fvta=0b4g^^)@+p3=6=g0sov1v1o9upu*y(ua^v3h3309uc4xm')
+# SECRET_KEY = 'django-insecure-fvta=0b4g^^)@+p3=6=g0sov1v1o9upu*y(ua^v3h3309uc4xm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['web-production-d23d4.up.railway.app']
+
 
 
 # Application definition
