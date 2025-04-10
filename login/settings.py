027c42b4929083b3785697f799f6730f08e475a9
josue24hs <josue24hs@gmail.com>
2025-04-09 18:07:21 -0600

    Configuración temporal con SQLite

--- login/settings.py
+++ login/settings.py
@@ -77,9 +77,12 @@ WSGI_APPLICATION = 'login.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
+
 
 
 # Password validation
